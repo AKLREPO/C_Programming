7923abddca265748b4edfc3250ed10c8a7122161
AKLREPO <53397893+AKLREPO@users.noreply.github.com>
2019-12-11 16:53:38 +0800

    Added Exercise 1

---
+++ Exercise_1/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/*This is a comment. */
+int main(int argh, char *argv[])
+{
+	int distance = 100;
+	//this is also a comment
+	printf("You are %d miles away.\n", distance);
+	printf("There is an car.\n");
+	printf("There is another car.\n");
+	printf("There is another cat.\n");
+	printf("There is a ferrari.\n");
+	printf("There is a ferrari3.\n");
+	return 0;
+}
